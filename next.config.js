b9eb529a3c7a64e775e6955705c15fd230459148
Carlos Barros <carlosbwebdev@gmail.com>
2023-04-24 10:15:24 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withPWA = require("next-pwa");
 
-module.exports = nextConfig
+const config = {
+  // here goes your Next.js configuration
+  reactStrictMode: true,
+};
+
+module.exports = withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+  scope: "/",
+  sw: "service-worker.js",
+})(config);
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,30 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link
+          rel="manifest"
+          href="/manifest.json"
+          crossOrigin="use-credentials"
+        />
+        <meta name="apple-touch-fullscreen" content="yes" />
+        <meta
+          name="viewport"
+          content="initial-scale=1, viewport-fit=cover, user-scalable=no"
+        />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <link rel="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes"></meta>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default App;
