c8c99e1369b1a23573f98e77df0aa3b31feb8e7f
Carlos Barros <carlosbwebdev@gmail.com>
2023-04-24 10:59:15 +0100

    uninstall next pwa and use pwa generator

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
-const withPWA = require("next-pwa");
+/** @type {import('next').NextConfig} */
 
-const config = {
-  // here goes your Next.js configuration
+const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = withPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-  scope: "/",
-  sw: "service-worker.js",
-})(config);
+module.exports = nextConfig;
